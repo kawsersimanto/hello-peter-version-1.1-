83587f8e33cb6b4305f28b1bbac769782c6b33d0
Kawser Simanto <kawsersimanto@gmail.com>
2022-03-01 01:25:40 +0600

    Add files via upload

---
+++ js/peter-video.js
@@ -0,0 +1,24 @@
+// video paly on button click 
+let home = document.querySelector(".home");
+let video = document.querySelector(".home video");
+let videoSrc = document.querySelector(".home video").getAttribute("src");
+let clickButton = document.querySelector(".home button");
+
+
+clickButton.addEventListener("click", displayVideo);
+function displayVideo(){
+    home.style.background = `url('../${videoSrc}')`;
+    video.style.display = "block";
+    clickButton.style.display = 'none';
+    video.addEventListener('ended', hideVideo);
+}
+
+function hideVideo(){
+    let style = home.style;
+    style.background = "url('/img/main/dr-octopus-1.jpg')";
+    style.backgroundRepeat = "no-repeat";
+    style.backgroundSize = "cover";
+    style.backgroundPosition = "center";
+    video.style.display = "none";
+    clickButton.style.display = 'block';
+}
